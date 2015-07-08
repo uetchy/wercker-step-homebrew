0befad4a1fd63d64779237d7f42a2a383740d57a
Yasuaki Uechi <uetchy@randompaper.co>
2015-07-08 20:08:31 +0900

    Add run script

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+SOLVER_VERSION="1.0.0"
+
+if [ ! -n "$WERCKER_HOMEBREW_TOKEN" ]; then
+  fail "Missing 'token' for Github API"
+fi
+
+# WERCKER_GIT_REPOSITORY = solver
+
+name="${WERCKER_HOMEBREW_NAME:-WERCKER_GIT_REPOSITORY}"
+owner="${WERCKER_HOMEBREW_OWNER:-WERCKER_GIT_OWNER}"
+# branch="${WERCKER_HOMEBREW_BRANCH:-master}"
+version="${WERCKER_HOMEBREW_VERSION}"
+message="${WERCKER_HOMEBREW_MESSAGE}"
+file64="${WERCKER_HOMEBREW_FILE64}"
+file32="${WERCKER_HOMEBREW_FILE32}"
+committer="werckerbot"
+committer_email="pleasemailus@wercker.com"
+
+curl -L https://github.com/uetchy/solver/releases/download/v${SOLVER_VERSION}/solver_linux_amd64.tar.gz -o ${WERCKER_STEP_ROOT}/solver_linux_amd64.tar.gz
+tar xzf solver_linux_amd64.tar.gz
+solver_bin=${WERCKER_STEP_ROOT}/solver_linux_amd64/solver
+
+${solver_bin} push \
+  --token $WERCKER_HOMEBREW_TOKEN \
+  --name $name \
+  --owner $owner \
+  --version $version \
+  --message $message \
+  --target-path-64 $file64 \
+  --target-path-32 $file32 \
+  --committer $committer \
+  --committer-email $committer_email
