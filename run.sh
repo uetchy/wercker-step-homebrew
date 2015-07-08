4623efd31f60ac8c60f293f2e95095b5b01fe01e
Yasuaki Uechi <uetchy@randompaper.co>
2015-07-08 21:41:05 +0900

    Fix bug

--- run.sh
+++ run.sh
@@ -23,11 +23,11 @@ solver_bin=${WERCKER_STEP_ROOT}/solver_linux_amd64/solver
 cd $WERCKER_SOURCE_DIR
 ${solver_bin} push \
   --token "${WERCKER_HOMEBREW_TOKEN}" \
-  --name "${$name}" \
-  --owner "${$owner}" \
-  --version "${$version}" \
-  --message "${$message}" \
+  --name "${name}" \
+  --owner "${owner}" \
+  --version "${version}" \
+  --message "${message}" \
   --target-path-64 "${file64}" \
   --target-path-32 "${file32}" \
-  --committer "${$committer}" \
-  --committer-email "${$committer_email}"
+  --committer "${committer}" \
+  --committer-email "${committer_email}"
